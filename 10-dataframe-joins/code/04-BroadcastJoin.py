de62e5f380dd9328c7d84f019b0456c97b6b36dc
Alok Shandilya <alok.shandilya10@gmail.com>
2025-04-10 13:53:07 +0530

    feat(broadcast-join): add code

---
+++ 10-dataframe-joins/code/04-BroadcastJoin.py
@@ -0,0 +1,42 @@
+from pyspark.sql import DataFrame, SparkSession
+from pyspark.sql.functions import broadcast
+
+# see data/df1, data/df2 (3 files each, to get 3 partitions)
+spark: SparkSession = (
+    # 3 parallel threads
+    SparkSession.builder.master("local[3]")  # type: ignore
+    .appName("ShuffleJoin")
+    .getOrCreate()
+)
+
+spark.conf.set("spark.sql.shuffle.partitions", "3")
+spark.conf.set("spark.sql.adaptive.enabled", "false")
+
+flight_time_df1: DataFrame = spark.read.format("json").load("data/d1/")
+flight_time_df2: DataFrame = spark.read.format("json").load("data/d2/")
+
+# flight_time_df1.show()
+# flight_time_df2.show()
+
+join_expr = flight_time_df1.id == flight_time_df2.id
+# join_df = flight_time_df1.join(flight_time_df2, join_expr, "inner")
+
+# use broadcast join
+join_df = flight_time_df1.join(broadcast(flight_time_df2), join_expr, "inner")
+
+# print("--- Query Explanation ---")
+# join_df.explain(extended=True)  # Use extended=True for most detail
+# print("-----------------------")
+
+# join is a transformation, we need an action to trigger it like show()
+# alfo foreach() is also an action.
+
+# .foreach() is an action in PySpark Structured Streaming that enables you to
+# define custom logic to write output of streaming query to external storage
+# systems.
+
+join_df.foreach(lambda f: None)
+# join_df.show()
+
+# see spark UI @ http://localhost:4040/jobs/
+input("Press Enter to continue...")  # for Spark UI
