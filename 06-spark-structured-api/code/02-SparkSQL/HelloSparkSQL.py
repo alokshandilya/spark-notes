561d6f05c3eb8b6ae7f3332a9a5bb87e2d3d140d
Alok Shandilya <alok.shandilya10@gmail.com>
2025-04-10 13:52:39 +0530

    refactor: code formatting

--- 06-spark-structured-api/code/02-SparkSQL/HelloSparkSQL.py
+++ 06-spark-structured-api/code/02-SparkSQL/HelloSparkSQL.py
@@ -1,11 +1,13 @@
 import sys
 
 from lib.logger import Log4J
-from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame, SparkSession
 
 if __name__ == "__main__":
-    spark = (
-        SparkSession.builder.master("local[3]").appName("HelloSparkSQL").getOrCreate()
+    spark: SparkSession = (
+        SparkSession.builder.master("local[3]")  # type: ignore
+        .appName("HelloSparkSQL")
+        .getOrCreate()
     )
 
     logger = Log4J(spark)
@@ -16,16 +18,20 @@ if __name__ == "__main__":
 
     logger.info(msg="Hello, Spark SQL!!")
 
-    # survey_df = (
-    #     spark.read.option("header", "true")
-    #     .option("inferSchema", "true")
-    #     .csv(sys.argv[1])
+    # survey_df: DataFrame = spark.read.csv(
+    #     sys.argv[1],
+    #     header=True,
+    #     inferSchema=True,
     # )
 
-    survey_df = spark.read.csv(sys.argv[1], header=True, inferSchema=True)
+    survey_df = (
+        spark.read.option("header", "true")
+        .option("inferSchema", "true")
+        .csv(sys.argv[1])
+    )
 
     survey_df.createOrReplaceTempView("survey_tbl")
-    count_df = spark.sql(
+    count_df: DataFrame = spark.sql(
         "SELECT country, count(1) as count FROM survey_tbl GROUP BY country"
     )
 
